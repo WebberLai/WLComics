09351409f071f172e7ed500f57e1ca6c16c6972b
Webber Lai <webberlai@gmail.com>
2017-09-05 17:48:06 +0800

    先加入聽藍芽鍵盤左右的按鍵事件

--- WLComics/AppDelegate.swift
+++ WLComics/AppDelegate.swift
@@ -12,7 +12,10 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDelegate {
 
     var window: UIWindow?
-
+    
+    // Define identifier
+    let notificationName = Notification.Name("BLERightClickNotification")
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         let splitViewController = window!.rootViewController as! UISplitViewController
@@ -24,7 +27,22 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDele
         WLComics.sharedInstance().setUp()
         return true
     }
-
+    
+    override var keyCommands: [UIKeyCommand]? {
+        return [
+            UIKeyCommand.init(input: UIKeyInputRightArrow , modifierFlags: .command, action: #selector(rightClick)),
+            UIKeyCommand.init(input: UIKeyInputLeftArrow , modifierFlags: .command, action: #selector(leftClick))
+        ]
+    }
+    
+    func rightClick(){
+        NotificationCenter.default.post(name: notificationName, object: UIKeyInputRightArrow)
+    }
+    
+    func leftClick(){
+        NotificationCenter.default.post(name: notificationName, object: UIKeyInputLeftArrow)
+    }
+    
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
