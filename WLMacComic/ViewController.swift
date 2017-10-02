02dbb3d82fc870b4e71b289e019a2cd8ec66dd90
Webber Lai <webberlai@gmail.com>
2017-10-02 15:52:19 +0800

    新加入Mac 版本的target，但是不知道怎麼寫哈哈

---
+++ WLMacComic/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  WLMacComic
+//
+//  Created by Webber Lai on 2017/10/2.
+//  Copyright © 2017年 webberlai. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
